2056720a34004312f8a2ceca0be34717e84d85e2
Roshan Krishnachandra Jha <roshanjha819@gmail.com>
2020-05-20 16:45:44 +0530

    db connections

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'postgresql-aerodynamic-90708',
-        user: 'postgres',
-        password: '',
-        database: 'smart_brain'
+        host: process.env.DATABASE_URL,
+        ssl:true
     }
 });
 
